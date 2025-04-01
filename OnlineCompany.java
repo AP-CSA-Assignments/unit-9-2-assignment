26046ebc5fff443563cc41da359647abeeee7225
Kyle Vu <ktvu@hbuhsd.edu>
2025-03-31 22:03:14 -0700

    Update OnlineCompany.java

--- OnlineCompany.java
+++ OnlineCompany.java
@@ -1,18 +1,24 @@
-public class OnlineCompany extends Company{
-
+public class OnlineCompany extends Company
+{
 	private String webAddress;
 
-	public OnlineCompany(String name, String webAddress){
+	// An online company's address is its webaddress
+	public OnlineCompany(String name, String webAddress)
+	{
 
 	}
 
-	//Return the website address
-
-	public String address(){
-
+	// Return the website address, since the address is
+	// reinterpreted as the web address here.
+	public String address()
+	{
+		return null;
 	}
 
-	/**
+	/** Return a String of the form
+ 	* [name]
+	* [webaddress]
+  *
 	* Remember To get name from superclass, use super.getName()
 	* 
 	* Example Output:
@@ -20,7 +26,8 @@ public class OnlineCompany extends Company{
 	* www.codehs.com
 	*/
 
-	public String toString(){
-
+	public String toString()
+	{
+		return null;
 	}
 }
